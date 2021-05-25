52617cb9645d041f49d67480a618851377a271c8
spadm22 <spadm22@gmail.com>
2021-05-25 12:54:22 +0530

    added for page show

--- src/main/java/com/pn/devOps/DevOpsApplication.java
+++ src/main/java/com/pn/devOps/DevOpsApplication.java
@@ -2,12 +2,22 @@ package com.pn.devOps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DevOpsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevOpsApplication.class, args);
 	}
 
+	@RequestMapping("/")
+	public String getWelcome(@RequestParam String name) {
+		return "Welcome "+name;
+	}
 }
