db29ca4a9f80fc642275ac5e8e06f96a52e64dd3
spadm22 <spadm22@gmail.com>
2021-05-25 12:39:06 +0530

    initial commit

---
+++ src/test/java/com/pn/devOps/DevOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pn.devOps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
