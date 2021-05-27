feb7c32ab35477eb18ee80ddd2c671f7a36a33be
Padmanabha Sahu <39075856+spadm22@users.noreply.github.com>
2021-05-27 14:59:18 +0530

    Just commited the sb pkgs

--- src/main/java/com/pn/devOps/DevOpsApplication.java
+++ src/main/java/com/pn/devOps/DevOpsApplication.java
@@ -1,6 +1,6 @@
 package com.pn.devOps;
 
-import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
